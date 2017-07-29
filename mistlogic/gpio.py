305576f942fd930cefb34d128f0edd0785b42e97
Jesus Flores <joy_warmgun@hotmail.com>
2017-07-28 20:04:16 -0500

    Creando biblioteca para manejo de gpios 	new file:   mistlogic/gpio.py

---
+++ mistlogic/gpio.py
@@ -0,0 +1,40 @@
+from mistlogic.config import *
+import RPi.GPIO as GPIO
+
+class Gpio(object):
+    def __init__(self, props):
+        self.__props = props
+        self.__gpio = Config('gpio', props)
+    
+    def declare(self):
+        
+        for key in self.__props:
+            prop = self.__gpio.getProp(key)
+            GPIO.setup(prop['pin'], GPIO.OUT)
+            GPIO.output(prop['pin'], prop['value'])
+
+    def setValue(self, name, value):
+        prop = self.__props[name]
+        prop['value'] = value
+        GPIO.output(prop['pin'], value)
+        self.__gpio.setProp(name, prop)
+
+    def high(self, name):
+        self.setValue(name, GPIO.HIGH)
+
+    def low(self, name):
+        self.setValue(name, GPIO.LOW)
+
+    def toggle(self, name):
+        prop = self.__props[name]
+
+        if prop['value']:
+            self.low(name)
+        else:
+            self.high(name)
+
+
+
+
+
+
