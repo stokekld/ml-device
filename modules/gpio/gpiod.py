3c724264785e497ba478694dd272b5ce5ca6320f
Jesus Flores <joy_warmgun@hotmail.com>
2017-07-31 11:00:54 -0500

    agregando excepcion en fifo creado 	modified:   modules/gpio/gpiod.py

--- modules/gpio/gpiod.py
+++ modules/gpio/gpiod.py
@@ -1,7 +1,6 @@
 from mistlogic.gpio import Gpio
 import RPi.GPIO as GPIO
-import atexit
-import os
+import os, stat, atexit
 
 GPIO.setmode(GPIO.BCM)
 
@@ -20,8 +19,10 @@ gpio.declare()
 
 thefifo = '/etc/mistlogic/gpio/gpio.fifo'
 
-if not os.path.isfile(thefifo):
+try:
     os.mkfifo(thefifo)
+except:
+    print "ya existe"
 
 def cleanup():
     os.remove(thefifo)
